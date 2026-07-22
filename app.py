4745929884fa3b007b45df014b256f6b0a5a60cb
void <toobasoomro27@gmail.com>
2026-07-22 22:11:25 +0500

    Add navbar

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    ...
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/search')
+def search():
+    return "search page"
+
+@app.route('/trending')
+def trending():
+    return "trending media type"
